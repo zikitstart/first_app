8e387e123d367ea2ccb858495df61eefea041b8d
zikit <zikit.start@yandex.ru>
2022-12-08 12:42:42 +0300

    Первый коммит.

---
+++ src/test/java/com/example/first_app/FirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
